a417c870535f5efb27e5e7223b604bb5b8946607
robbie <robbiedyson@robbies-mbp.mynetworksettings.com>
2024-07-08 12:34:07 -0400

    adsense test 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
     const script = document.createElement("script");
     script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2496238868363436"
     script.async = true;
+    document.body.append(script);
   })
 </script>
 
