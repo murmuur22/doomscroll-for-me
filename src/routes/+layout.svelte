9bda52b2a790c2c59823a7f8b91bbb869498894c
robbie <robbiedyson@robbies-mbp.mynetworksettings.com>
2024-07-08 12:21:33 -0400

    adsense test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
   import "../app.css";
+  import { onMount } from "svelte";
+
+  onMount(()=> {
+    const script = document.createElement("script");
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2496238868363436"
+    script.async = true;
+  })
 </script>
 
 <slot />
