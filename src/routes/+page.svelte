230633a4c8bb80df8da0cfbf98fa016b526a08bb
robbie <robbiedyson@robbies-mbp.mynetworksettings.com>
2024-07-08 11:36:19 -0400

    adsterra test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script async="async" data-cfasync="false" src="//pl23729903.highrevenuenetwork.com/0b7a5c3cd5154b72a1b3a0556ca4dfd2/invoke.js"></script>
+<div id="container-0b7a5c3cd5154b72a1b3a0556ca4dfd2"></div>
