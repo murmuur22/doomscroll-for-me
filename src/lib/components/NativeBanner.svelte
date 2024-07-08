757f62609a16e81e00e9999e52f485d24c50afe2
robbie <robbiedyson@robbies-mbp.mynetworksettings.com>
2024-07-08 13:23:57 -0400

    added native banner sveltified

---
+++ src/lib/components/NativeBanner.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let banner: HTMLElement;
+
+  const options: any = {
+    key: '0b7a5c3cd5154b72a1b3a0556ca4dfd2',
+  };
+
+  onMount(() => {
+    if (!banner.firstChild) {
+      const script = document.createElement('script');
+      const div = document.createElement('div');
+      script.type = 'text/javascript';
+      script.src = `//pl23729903.highrevenuenetwork.com/${options.key}/invoke.js`;
+      div.id = `container-${options.key}`;
+
+      if (banner) {
+        banner.append(div);
+        banner.append(script);
+      }
+    }
+  });
+</script>
+
+<div bind:this={banner}></div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
 import SocialBar from '$lib/components/SocialBar.svelte'
+import NativeBanner from "$lib/components/NativeBanner.svelte"
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
 <SocialBar />
+<SocialBar />
+<SocialBar />
+
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
+<NativeBanner />
